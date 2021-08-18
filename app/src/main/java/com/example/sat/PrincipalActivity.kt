19d704a246e8c5bfcf6f8cb77ee2f5dad6418948
Nicolasanaya <juan.anaya.2017@upb.edu.co>
2021-08-17 19:12:14 -0500

    segundo commit

--- app/src/main/java/com/example/sat/PrincipalActivity.kt
+++ app/src/main/java/com/example/sat/PrincipalActivity.kt
@@ -2,10 +2,26 @@ package com.example.sat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class PrincipalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal2)
+
+        val bottomNavigationView  = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+//        val navController : NavController = findNavController(R.id.fragment)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragmento,R.id.notificacionesFragmento,R.id.perfilFragmento,R.id.reportesFragmento,R.id.tareasFragmento))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
