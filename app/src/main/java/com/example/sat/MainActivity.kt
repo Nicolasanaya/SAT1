39d1c147fdf2e9e6049dbc4d33ad8191f30e23d8
Nicolasanaya <juan.anaya.2017@upb.edu.co>
2021-08-25 17:29:26 -0500

    Cuarto Commit

--- app/src/main/java/com/example/sat/MainActivity.kt
+++ app/src/main/java/com/example/sat/MainActivity.kt
@@ -3,9 +3,18 @@ package com.example.sat
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
+    var correo : Boolean = false
+    var contraseña :Boolean = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,10 +25,39 @@ class MainActivity : AppCompatActivity() {
 
 
     fun BtnIniciar_Click(view: View) {
+        if (!Patterns.EMAIL_ADDRESS.matcher(etUsuario.text.toString()).matches()){
+            etUsuario.error = "Correo Erroneo"
+            correo = false
+        }else{
+            correo = true
+            etUsuario.error=null
+        }
 
-        val intento1 = Intent(this, PrincipalActivity::class.java)
-        startActivity(intento1)
+        //val p:Pattern = Pattern.compile("[^(?=\\w*\\d)(?=\\w*[A-Z])(?=\\w*[a-z])\\S{8,16}\$]")
+        if (etClave.length() == 0){
+            etClave.error = "Digite la Clave"
+            contraseña = false
+        }else{
+            contraseña = true
+            etClave.error=null
+        }
+
+        if (correo && contraseña){
+            val user:String = etUsuario.text.toString()
+            val pass:String = etClave.text.toString()
+            if (user.equals("nicolasanaya_s@hotmail.com")&&pass.equals("Hola12345")){
+                val intento1 = Intent(this, PrincipalActivity::class.java)
+                startActivity(intento1)
+            }else{
+                Toast.makeText(applicationContext,"Usuario o Contraseña Incorrecta", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+//        val intento1 = Intent(this, PrincipalActivity::class.java)
+//        startActivity(intento1)
 
     }
+
+
 
 }
--- app/src/main/java/com/example/sat/reportesFragmento.kt
+++ app/src/main/java/com/example/sat/reportesFragmento.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_reportes_fragmento.*
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +20,11 @@ private const val ARG_PARAM2 = "param2"
  * Use the [reportesFragmento.newInstance] factory method to
  * create an instance of this fragment.
  */
-class reportesFragmento : Fragment() {
+class reportesFragmento : Fragment(), View.OnClickListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,12 +35,17 @@ class reportesFragmento : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reportes_fragmento, container, false)
+        val view: View = inflater!!.inflate(R.layout.fragment_reportes_fragmento, container, false)
+        val btn: Button = view.findViewById(R.id.btnEnviar)
+        btn.setOnClickListener(this)
+        return view
     }
+
 
     companion object {
         /**
@@ -55,5 +65,20 @@ class reportesFragmento : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btnEnviar -> {
+                Toast.makeText(context, "Usuario o Contraseña Incorrecta", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            else -> {
+            }
+
+        }
+
+
     }
 }
